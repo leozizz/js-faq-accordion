8cd7f39167f29006c7ddf099708dcfeb9785775d
leozizz <leonardocps2015@gmail.com>
2022-09-09 15:46:11 -0300

    Testing new structure

---
+++ assets/script/script.js
@@ -0,0 +1,24 @@
+// const botao = document.querySelectorAll(".botao")
+// const painel = document.querySelectorAll(".painel")
+// let i = 0
+
+// for (let i = 0; i < botao.length; i++) {
+//     funcao()
+// }
+
+// function funcao() {
+//     let = respostaAtual = i - 1
+//     botao[i].addEventListener("click", function () {
+//         /* "toggle" troca entre adicionar e remover a classe "ativo" que irá deixar o botão clicado em destaque*/
+//         this.classList.remove("ativo")
+//         this.classList.add("ativo")
+
+//         //Faz a troca entre esconder e mostrar o painel ativo
+//         let painel = this.nextElementSibling
+//         if (painel.style.display === "block") {
+//             painel.style.display = "none"
+//         } else {
+//             painel.style.display = "block"
+//         }
+//     })
+// }
